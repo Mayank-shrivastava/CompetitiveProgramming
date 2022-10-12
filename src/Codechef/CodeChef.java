9a0e533afd8783a9ed00968f81de1b50c8caa378
Mayank Shrivastava <mayank.shrivastava_cs19@gla.ac.in>
2022-10-13 00:43:04 +0530

    initial commit

---
+++ src/Codechef/CodeChef.java
@@ -0,0 +1,16 @@
+package Codechef;
+
+import java.util.*;
+
+public class CodeChef {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		System.out.println(t);
+		System.out.println("Hello world");
+		System.out.println("123");
+		scn.close();
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mayan
+ *
+ */
+module CompetitiveProgrammingPractice {
+}
